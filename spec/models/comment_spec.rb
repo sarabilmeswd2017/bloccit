2583f8c308a8ab6eda808e3f875de5b80ba68fff
Sara Bilmes <saraesrig@gmail.com>
2017-07-25 10:13:00 +0300

    Assignment-rails-14:complete

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:post) { Post.create!(title: "New Post Title", body: "New Post Body") }
-     let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+  let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
      describe "attributes" do
        it "has a body attribute" do
