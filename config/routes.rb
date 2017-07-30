2bbf60e0050ed7f37fe7825834efcf83d4fb1e00
Sara Bilmes <saraesrig@gmail.com>
2017-07-30 12:00:58 +0300

    Assignment-rails-20:not complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-#  get 'advertisements/index'
-
-#  get 'advertisements/show'
-
-#  get 'advertisements/new'
-
-#  get 'advertisements/create'
+  resources :questions
 
   resources :advertisements
 
