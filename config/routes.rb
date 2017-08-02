b848208a6a80ad927f812f11b7e114702637d89d
Sara Bilmes <saraesrig@gmail.com>
2017-08-02 10:57:25 +0300

    Checkpoint-rails-23:complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  
+
 
   resources :topics do
      resources :posts, except: [:index]
      resources :sponsored_posts, except: [:index]
    end
 
+  resources :users, only: [:new, :create]
+  
   resources :questions
 
   resources :advertisements
