9e143a75b02b7761565b797088a999a84218cc7e
Sara Bilmes <saraesrig@gmail.com>
2017-08-06 11:20:22 +0300

    Checkpoint-rails-27:Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@ require 'random_data'
 
 include RandomData
 
+5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
 15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -14,7 +24,7 @@ include RandomData
  50.times do
  # #1
    Post.create!(
- # #2
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -52,11 +62,18 @@ include RandomData
   )
 end
 
+user = User.first
+ user.update_attributes!(
+   email: 'saraesrig@gmail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+
  puts "#{Post.count}"
  Post.find_or_create_by(title:"A unique title", body:"A unique body")
  puts "#{Post.count}"
 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{SponsoredPost.count} sponsored posts created"
  puts "#{Post.count} posts created"
