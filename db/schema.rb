d78f3b80d294484b22dc6854546a7be23a3426de
Sara Bilmes <saraesrig@gmail.com>
2017-08-10 08:38:32 +0300

    Checkpoint-28-fixing-bugs:Complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806063943) do
+ActiveRecord::Schema.define(version: 20170809192859) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20170806063943) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -62,12 +62,20 @@ include RandomData
   )
 end
 
-user = User.first
- user.update_attributes!(
-   email: 'saraesrig@gmail.com', # replace this with your personal email
-   password: 'helloworld'
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
  )
 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+ 
  puts "#{Post.count}"
  Post.find_or_create_by(title:"A unique title", body:"A unique body")
  puts "#{Post.count}"
