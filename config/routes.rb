8e21c7b53f911f16deabbdcbba26c9942222a081
Sara Bilmes <saraesrig@gmail.com>
2017-08-13 11:59:52 +0300

    Checkpoint-31-new:Complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
    resources :posts, only: [] do
      resources :comments, only: [:create, :destroy]
+     resources :favorites, only: [:create, :destroy]
    end
 
 
